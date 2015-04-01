b7fb9485cbe1de2fb068a3532f0d1b4f04292cfc
igorgarciateran <igorgt@gmail.com>
2015-04-01 19:33:29 +0200

    hoy

---
+++ Geolocation/app/src/main/java/com/igorgarcia/geolocation/Geolocaclizacion/Geo_Devuelve.java
@@ -0,0 +1,44 @@
+package com.igorgarcia.geolocation.Geolocaclizacion;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+/**
+ * Created by cursomovil on 1/04/15.
+ */
+public class LocationListenerX implements android.location.LocationListener {
+
+
+    public interface AddLocationInterface {
+        public void AddLocation(Location location);
+    }
+
+
+    private AddLocationInterface target;
+    public LocationListenerX (AddLocationInterface target) {
+        this.target=target;
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
--- Geolocation/app/src/main/java/com/igorgarcia/geolocation/MainActivity.java
+++ Geolocation/app/src/main/java/com/igorgarcia/geolocation/MainActivity.java
@@ -1,39 +1,90 @@
 package com.igorgarcia.geolocation;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.igorgarcia.geolocation.Geolocaclizacion.LocationListenerX;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LocationListenerX {
+
+    //Hay que pedir permisos en el manifiesto, sino no funciona
+
+    private TextView lblLatitud;
+    private TextView lblLongitud;
+    private TextView lblAltitud;
+    private TextView lblVelocidad;
+
+    private LocationManager locationManager = LocationManager;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        lblLatitud = (TextView) findViewById(R.id.LblLatitud);
+        lblLongitud = (TextView) findViewById(R.id.LblLongitud);
+        lblAltitud = (TextView) findViewById(R.id.LblAltitud);
+        lblVelocidad = (TextView) findViewById(R.id.LblVelocidad);
+
+
+        getLocationProvider();
+        listenLocationChanges();
+
+
     }
+
+    private void getLocationProvider() {
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        Criteria criterios = new Criteria();
+
+        criterios.setAccuracy(Criteria.ACCURACY_FINE);
+        criterios.setSpeedRequired(true);
+        criterios.setAltitudeRequired(true);
+
+
+        String bestProvider = locationManager.getBestProvider(criterios, true);
+
+        Log.d("GEO", bestProvider);
+    }
+
+
+    private void listenLocationChanges(){
+
+
+        int t= 5000;
+        int distance=5;
+
+        LocationListenerX listener= new LocationListenerX (this);
+
+
+        locationManager=requesLocationUpdates(provider,t,distance,listener);
+
+    }
+
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void AddLocation(Location location){
+        lblLatitud.setText(String.valueOf(location.getLatitude));
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
